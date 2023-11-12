bff10f2b8c4777a487d69b37fcacdb4a63d56677
RudneyAlbuquerque <rudneyceara@yahoo.com.br>
2023-11-12 12:51:05 -0300

    Acordeon Education added updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,19 @@ function updatePortfolio(profileData) {
     }).join('')
 }
 
+function updateEducationExperience(profileData) {
+    const EducationExperience = document.getElementById('profile.EducationExperience')
+    EducationExperience.innerHTML = profileData.EducationExperience.map(experience => {
+        return `
+            <li>
+                <h3 class="title">${experience.name}</h3>
+                <p class="period">${experience.period}</p>
+                <p>${experience.institution}</p>
+            </li>
+        `
+    }).join('')
+}
+
 function updateProfessionalExperience(profileData) {
     const professionalExperience = document.getElementById('profile.professionalExperience')
     professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
@@ -69,5 +82,6 @@ function updateProfessionalExperience(profileData) {
     updateHardSkills(profileData)
     updateLanguages(profileData)
     updatePortfolio(profileData)
+    updateEducationExperience(profileData)
     updateProfessionalExperience(profileData)
 })()
